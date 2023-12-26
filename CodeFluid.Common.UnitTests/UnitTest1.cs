71eec5c033cbb5e91be6828f8417ee2d5ca02d33
Justin Minnaar <JustinMinnaar@users.noreply.github.com>
2023-12-26 07:58:34 +0200

    Add project files.

---
+++ CodeFluid.Common.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CodeFluid.Common.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ CodeFluid.Common/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CodeFluid.Common;
+
+public class Class1
+{
+
+}
